5365d6147a0cdd58bdd2fd417d9ecb25149339dd
Hussein Omar <husseinomar6190@gmail.com>
2022-04-09 20:15:50 +0300

    adding custom output event bidding in quote form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Quote } from '../quote';
 
@@ -11,17 +11,25 @@ export class QuoteFormComponent implements OnInit {
   publisher: string;
   quote: string;
   author: string;
+  newQuote: Quote;
 
-  onSubmit(form: NgForm) {
-    // console.log(form);
-    // console.log(form.value);
-    // console.log(form.value.author);
+  @Output() pushQuote: EventEmitter<Quote> = new EventEmitter();
+
+  onSubmitPublish(form: NgForm) {
     console.log(form.value.description);
     console.log(form.value.publisher);
     this.publisher = form.value.publisher;
     this.quote = form.value.description;
     this.author = form.value.author;
 
+    this.newQuote = new Quote(
+      this.quote,
+      this.publisher,
+      this.author,
+      new Date()
+    );
+
+    this.pushQuote.emit(this.newQuote);
     form.reset();
   }
 
