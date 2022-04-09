4437474d468f88bea108b5f796424abaab15c39d
Hussein Omar <husseinomar6190@gmail.com>
2022-04-09 18:30:47 +0300

    adding a hard coded data to quote.component,ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,15 @@
 export class Quote {
+  showDescription: boolean;
+  upvotes: number;
+  downvotes: number;
+  constructor(
+    public content: string,
+    public publisher: string,
+    public author: string,
+    public datePublished: Date
+  ) {
+    this.showDescription = false;
+    this.upvotes = 0;
+    this.downvotes = 0;
+  }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
 })
 export class QuoteComponent implements OnInit {
+  allQuotes: Quote[] = [
+    new Quote('This is good', 'Hussein', 'Maclcom X', new Date()),
+    new Quote('This is bad', 'hassan', 'Maclcom z', new Date()),
+    new Quote('This is best', 'siham', 'Maclcom A', new Date()),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
