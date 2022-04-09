456860e2a01fd5c995c1b9b8407f12075866da19
Hussein Omar <husseinomar6190@gmail.com>
2022-04-09 17:58:58 +0300

    adding form validation and display errors

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,9 +7,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./quote-form.component.css'],
 })
 export class QuoteFormComponent implements OnInit {
+  publisher: string;
+  quote: string;
+  author: string;
+
   onSubmit(form: NgForm) {
     console.log(form);
     console.log(form.value);
+    console.log(form.value.author);
+    console.log(form.value.description);
+    console.log(form.value.publisher);
+    this.publisher = form.value.publisher;
+    this.quote = form.value.description;
+    this.author = form.value.author;
+
+    form.reset();
   }
 
   constructor() {}
