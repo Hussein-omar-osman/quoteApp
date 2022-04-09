8da26f701ce020baa5153b66966a0c95e0ed6ce3
Hussein Omar <husseinomar6190@gmail.com>
2022-04-09 21:12:19 +0300

    creating quote-detals component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { FormsModule } from '@angular/forms';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
-  declarations: [AppComponent, QuoteComponent, QuoteFormComponent],
+  declarations: [AppComponent, QuoteComponent, QuoteFormComponent, QuoteDetailsComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
